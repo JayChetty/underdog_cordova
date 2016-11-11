34ea7aac4d490e9d17913bd9362202e012cc298e
Jay Chetty <jaypaulchetty@gmail.com>
2016-11-11 08:52:27 +0000

    app working

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -19,6 +19,8 @@
 var app = {
     // Application Constructor
     initialize: function() {
+        window.isCordovaApp = true
+        console.log('initializing')
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
 
@@ -27,19 +29,48 @@ var app = {
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
-        this.receivedEvent('deviceready');
+        console.log("navigator.notification", navigator.notification)
+        // use this in app.js for urls and messenging
+        console.log("trying to set up firebase", firebase)
+        var config = {
+          apiKey: "AIzaSyAzIdDH3G6RkLKFNkZP3xjRnQgjmSD6Upc",
+          authDomain: "underdog-messenger.firebaseapp.com",
+          databaseURL: "https://underdog-messenger.firebaseio.com",
+          storageBucket: "underdog-messenger.appspot.com",
+          messagingSenderId: "861414133939"
+        };
+        firebase.initializeApp(config);
+
+        // FCMPlugin.getToken(
+        //   function(token){
+        //     console.log('got token', token)
+        //     FCMPlugin.subscribeToTopic('group_1');
+        //   },
+        //   function(err){
+        //     console.log('error retrieving token: ' + err);
+        //   }
+        // )
+
+
+        // )
+        // this.receivedEvent('deviceready');
+
+
+        // console.log("window", window)
+
+        window.start();
     },
 
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+        // var parentElement = document.getElementById(id);
+        // var listeningElement = parentElement.querySelector('.listening');
+        // var receivedElement = parentElement.querySelector('.received');
+        //
+        // listeningElement.setAttribute('style', 'display:none;');
+        // receivedElement.setAttribute('style', 'display:block;');
+        //
+        // console.log('Received Event: ' + id);
     }
 };
 
