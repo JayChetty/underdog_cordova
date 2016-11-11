28badc06b105844e2e9cd5886f162c74d3b16b33
Jay Chetty <jaypaulchetty@gmail.com>
2016-11-11 09:08:02 +0000

    only start cordova when it is ready try

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -19,19 +19,13 @@
 var app = {
     // Application Constructor
     initialize: function() {
-        window.isCordovaApp = true
         console.log('initializing')
+        window.isCordovaApp = true
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
     onDeviceReady: function() {
-        console.log("navigator.notification", navigator.notification)
-        // use this in app.js for urls and messenging
-        console.log("trying to set up firebase", firebase)
+        console.log("on device ready")
         var config = {
           apiKey: "AIzaSyAzIdDH3G6RkLKFNkZP3xjRnQgjmSD6Upc",
           authDomain: "underdog-messenger.firebaseapp.com",
@@ -40,37 +34,7 @@ var app = {
           messagingSenderId: "861414133939"
         };
         firebase.initializeApp(config);
-
-        // FCMPlugin.getToken(
-        //   function(token){
-        //     console.log('got token', token)
-        //     FCMPlugin.subscribeToTopic('group_1');
-        //   },
-        //   function(err){
-        //     console.log('error retrieving token: ' + err);
-        //   }
-        // )
-
-
-        // )
-        // this.receivedEvent('deviceready');
-
-
-        // console.log("window", window)
-
-        window.start();
-    },
-
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        // var parentElement = document.getElementById(id);
-        // var listeningElement = parentElement.querySelector('.listening');
-        // var receivedElement = parentElement.querySelector('.received');
-        //
-        // listeningElement.setAttribute('style', 'display:none;');
-        // receivedElement.setAttribute('style', 'display:block;');
-        //
-        // console.log('Received Event: ' + id);
+        // window.start();
     }
 };
 
